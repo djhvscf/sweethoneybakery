67d130d55a13e89cf2b5ca0263d6bb25065a5f02
Dennis Hernández <dennishernandezvargas@gmail.com>
2014-12-06 19:25:31 -0600

    Lista de ingredientes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,56 @@
 var datain = 
 [
     {
+		"id": "1",
         "nombre": "Azucar",
 		"cantidad": "5kg",
         "precio": "3190",
 		"precioUnidad": "0.64"
     },
     {
+		"id": "2",
         "nombre": "Sal",
 		"cantidad": "500g",
         "precio": "190",
 		"precioUnidad": "0.4"
     },
     {
+		"id": "3",
         "nombre": "Harina",
 		"cantidad": "1kg",
         "precio": "700",
 		"precioUnidad": "0.7"
     },
     {
+		"id": "4",
         "nombre": "Polvo de hornear",
 		"cantidad": "90g",
         "precio": "475",
 		"precioUnidad": "5..3"
     },
     {
+		"id": "5",
         "nombre": "Mantequilla",
 		"cantidad": "4b",
         "precio": "3100",
 		"precioUnidad": "775"
     },
     {
+		"id": "6",
         "nombre": "Huevos",
 		"cantidad": "30u",
         "precio": "3500",
 		"precioUnidad": "120"
     },
     {
+		"id": "7",
         "nombre": "Vainilla",
 		"cantidad": "950g",
         "precio": "3858",
 		"precioUnidad": "4.06"
     },
     {
+		"id": "8",
         "nombre": "Coco",
 		"cantidad": "100g",
         "precio": "545",
@@ -53,6 +61,8 @@ var datain =
 
 $(function () {
 	loadBootstrapTable(datain);
+	//populateIngredientes();
+	test();
 });
 
 var loadBootstrapTable = function (data) {
@@ -68,49 +78,41 @@ var loadBootstrapTable = function (data) {
 	});
 };
 
-var flatten = function(data) {
-    var result = {};
-    function recurse (cur, prop) {
-        if (Object(cur) !== cur) {
-            result[prop] = cur;
-        } else if (Array.isArray(cur)) {
-             for(var i=0, l=cur.length; i<l; i++)
-                 recurse(cur[i], prop ? prop+"."+i : ""+i);
-            if (l == 0)
-                result[prop] = [];
-        } else {
-            var isEmpty = true;
-            for (var p in cur) {
-                isEmpty = false;
-                recurse(cur[p], prop ? prop+"."+p : p);
-            }
-            if (isEmpty)
-                result[prop] = {};
-        }
-    }
-    recurse(data, "");
-    return result;
+var populateIngredientes = function ()
+{
+	//var selecct = $("#listaIngredientes");
+	var selecct = $('<select>').appendTo('.select');
+	for(var i = 0; i < datain.length; i++)
+	{
+		selecct.append('<option value=' + datain[i].id + '>' + datain[i].nombre + '</option>');
+	}
+	
+	
+	//return selecct;
 };
 
-var unflatten = function(data) {
-    "use strict";
-    if (Object(data) !== data || Array.isArray(data))
-        return data;
-    var result = {}, cur, prop, idx, last, temp;
-    for(var p in data) {
-        cur = result, prop = "", last = 0;
-        do {
-            idx = p.indexOf(".", last);
-            temp = p.substring(last, idx !== -1 ? idx : undefined);
-            cur = cur[prop] || (cur[prop] = (!isNaN(parseInt(temp)) ? [] : {}));
-            prop = temp;
-            last = idx + 1;
-        } while(idx >= 0);
-        cur[prop] = data[p];
-    }
-    return result[""];
-};	
-
-var getImg = function(value) {
-	return '<img src="'+ value + ' width="50px" height="50px">';
-};
+var test = function()
+{
+	$("#tblData tbody")
+		.append( "<tr>"+ 
+					"<td class='select'>"+
+						populateIngredientes()+
+					"</td>"+ 
+					"<td>"+
+						"<input type='text'/>"+
+					"</td>"+ 
+					"<td>"+
+						"<input type='text'/>"+
+					"</td>"+ 
+					"<td>"+
+						"<input type='text'/>"+
+					"</td>"+ 
+					"<td>"+
+						"<img src='images/disk.png' class='btnSave'>"+
+						"<img src='images/delete.png' class='btnDelete'/>"+
+					"</td>"+ 
+				"</tr>");
+	
+	//$(".btnSave").bind("click", Save);	
+	//$(".btnDelete").bind("click", Delete); };
+}
